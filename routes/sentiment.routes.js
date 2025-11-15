acfbe24c3e5a1e240c8f1e6110698330ca87175a
charan-crafts <mangalacharancharan@gmail.com>
2025-11-15 16:38:53 +0530

    fetched the commits from the apify

--- routes/sentiment.routes.js
+++ routes/sentiment.routes.js
@@ -5,8 +5,12 @@ const router = express.Router();
 
 const sentimentController = require('../controller/sentiment');
 
+const webscraperController = require('../controller/webscraper,js');
 // Analyze  comment
 router.post('/analyze', sentimentController.sentiment);
+
+// get the comments from instagram post
+router.post('/scrape-comments', webscraperController.scrapeComments);
 
 
 module.exports = router;
