fa24e011fc3e3e4ca9c8f4122ff37d85cbf39235
charan-crafts <mangalacharancharan@gmail.com>
2025-11-16 08:31:30 +0530

    updated the backend url

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const cookieParser = require('cookie-parser');
 connectDB();
 
 app.use(cors({
-    origin: process.env.FRONTEND_URL || ["http://localhost:8080", "http://localhost:5173"],
+    origin: process.env.FRONTEND_URL || ["http://localhost:8080", "https://brandpulse-ui-suite.vercel.app"],
     credentials: true,
 }));
 
